3f53e0885efd385263369e01707d330507c096a6
JOA021 <jacobovallealvarez@gmail.com>
2024-03-07 16:11:21 -0500

    avance

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { FirtsPageComponent } from './page/firts-page/firts-page.component';
 import { RegisterComponent } from './page/register/register.component';
 import { AboutUsComponent } from './page/about-us/about-us.component';
 import { AdminPageComponent } from './page/admin-page/admin-page.component'; 
+import { AddAdministratorComponent } from './page/add-administrator/add-administrator.component';
+import { AddTeacherComponent } from './page/add-teacher/add-teacher.component';
+import { AddStudentComponent } from './page/add-student/add-student.component';
 
 export const routes: Routes = [
     {path: "", component: FirtsPageComponent},
     {path: "registro", component: RegisterComponent},
     {path: "nosotros", component: AboutUsComponent},
     {path: "admin", component: AdminPageComponent},
+    {path: "addAdmin", component: AddAdministratorComponent},
+    {path: "addTeacher", component: AddTeacherComponent},
+    {path: "addStudent", component: AddStudentComponent},
 ]
---
+++ frontend/src/app/page/add-administrator/add-administrator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-administrator',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-administrator.component.html',
+  styleUrl: './add-administrator.component.css'
+})
+export class AddAdministratorComponent {
+
+}
